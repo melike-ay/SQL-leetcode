d3d406f930ca7e5de5c00edb8f81295b23ce104f
Melike Ay <136972965+melike-ay@users.noreply.github.com>
2025-02-22 16:35:57 +0300

    Add files via upload

---
+++ swap_seats.sql
@@ -0,0 +1,36 @@
+-- Problem:
+-- Given the table "Seat":
+-- +----+---------+
+-- | id | student |
+-- +----+---------+
+-- | 1  | Abbot   |
+-- | 2  | Doris   |
+-- | 3  | Emerson |
+-- | 4  | Green   |
+-- | 5  | Jeames  |
+-- +----+---------+
+
+-- Expected Output:
+-- +----+---------+
+-- | id | student |
+-- +----+---------+
+-- | 1  | Doris   |
+-- | 2  | Abbot   |
+-- | 3  | Green   |
+-- | 4  | Emerson |
+-- | 5  | Jeames  |
+-- +----+---------+
+-- Swap the seat ID of every two consecutive students.
+-- If the number of students is odd, the last student's ID remains unchanged.
+-- The result table should be ordered by ID in ascending order.
+
+-- ANSWER SQL Query:
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id, 
+    student
+FROM Seat
+ORDER BY id;
